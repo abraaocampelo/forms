3f28fdfd4a5f8ad2af3ceff033004d1fc111a464
abraaocampelo <abraao22690486@edum.rn.senac.br>
2023-01-24 11:32:03 -0300

    Pagina sobre

--- app/routes.py
+++ app/routes.py
@@ -5,20 +5,20 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', titulo = 'Home')
 
 @app.route('/contato', methods=['GET','POST'])
 def contato():
     form = LoginForm()
     if form.validate_on_submit():
+        print  ("Chegou aqui")
         mensagem = flash('A mensagem foi enviada com sucesso.')
-        return redirect('/index')
-    return render_template('contato.html', form=form)
+    return render_template('contato.html', form=form, titulo = 'Contato')
         
 
-@app.route('/features')
+@app.route('/sobre')
 def features():
-    return render_template('features.html')
+    return render_template('sobre.html', titulo = 'Sobre')
 
 
 
